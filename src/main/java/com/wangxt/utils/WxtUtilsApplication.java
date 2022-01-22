3cb1f013578f657da97c292503d262493da1e197
wangxt <wangxt@ckt.cn>
2022-01-22 15:46:54 +0800

    init

---
+++ src/main/java/com/wangxt/utils/WxtUtilsApplication.java
@@ -0,0 +1,13 @@
+package com.wangxt.utils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxtUtilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WxtUtilsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wangxt/utils/WxtUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wangxt.utils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WxtUtilsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
